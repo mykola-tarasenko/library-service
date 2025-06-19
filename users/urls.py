1aec03f27d85c25e4ccef1ebeae7ab47d4e53170
Mykola Tarasenko <nikolajtarasenko71@gmail.com>
2025-06-19 23:37:03 +0300

    Add url mapping for CreateUserView

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+from users.views import CreateUserView
+
 urlpatterns = [
+    path("", CreateUserView.as_view(), name="create"),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
