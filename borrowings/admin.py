0a6d5e8c22721223b194c0a36a7a2ee1a26a3bab
Mykola Tarasenko <nikolajtarasenko71@gmail.com>
2025-06-20 19:32:41 +0300

    Add Borrowing model in admin panel

--- borrowings/admin.py
+++ borrowings/admin.py
@@ -1,3 +1,15 @@
+# Register your models here.
 from django.contrib import admin
 
-# Register your models here.
+from borrowings.models import Borrowing
+
+
+@admin.register(Borrowing)
+class CityAdmin(admin.ModelAdmin):
+    list_display = (
+        "book",
+        "user__email",
+        "borrow_date",
+        "expected_return_date",
+        "actual_return_date",
+    )
