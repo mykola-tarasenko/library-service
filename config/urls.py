ae7a4f2bd3c0d40749b7dd2843cbadda1ecc1746
Mykola Tarasenko <nikolajtarasenko71@gmail.com>
2025-06-26 21:39:07 +0300

    Install drf-spectacular & set config

--- config/urls.py
+++ config/urls.py
@@ -18,10 +18,26 @@ Including another URLconf
 from debug_toolbar.toolbar import debug_toolbar_urls
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("books.urls", namespace="books")),
     path("api/", include("borrowings.urls", namespace="borrowings")),
     path("api/users/", include("users.urls", namespace="users")),
+    path("api/doc/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/doc/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "api/doc/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
 ] + debug_toolbar_urls()
