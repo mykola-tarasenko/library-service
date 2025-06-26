f094c4591978371ab57938322bfdd4ab9661b09f
Mykola Tarasenko <nikolajtarasenko71@gmail.com>
2025-06-26 19:44:58 +0300

    Add BookTest

--- books/tests.py
+++ books/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
-# Create your tests here.
+from books.models import Book
+
+
+class BookTest(TestCase):
+    def test_str_method(self):
+        book = Book.objects.create(
+            title="Test",
+            author="Test Testenko",
+            cover="SOFT",
+            inventory=2,
+            daily_fee=0.02,
+        )
+        self.assertEqual(
+            str(book),
+            f"{book.title} by {book.author} ({book.cover})",
+        )
